41d0a8f048412801f396a93a59aa11d553c75001
lordlamer <lordlamer@lordlamer.de>
2012-11-05 23:12:45 +0100

    * added download action

--- app/controllers/FileController.php
+++ app/controllers/FileController.php
@@ -25,7 +25,27 @@ class FileController extends Zend_Controller_Action
 
     public function downloadAction()
     {
+	$this->_helper->layout()->disableLayout();
+	$this->_helper->viewRenderer->setNoRender(true);
+
         // action body
+	// normal download
+	// with x-sendfile
+	// @see: http://codeutopia.net/blog/2009/03/06/sending-files-better-apache-mod_xsendfile-and-php/
+	// @see: http://redmine.lighttpd.net/projects/1/wiki/X-LIGHTTPD-send-file
+	// @see: http://wiki.nginx.org/XSendfile
+
+	$file = new Knowledgeroot_File($this->_getParam('id'));
+
+	header("Content-Type: " . $file->getType() . "; name=\"".$file->getName()."\"");
+	header("Content-Disposition: attachment; filename=\"".$file->getName()."\";");
+	header("Pragma: private");
+	header("Expires: 0");
+	header("Cache-Control: private, must-revalidate, post-check=0, pre-check=0");
+	header("Content-Transfer-Encoding: binary");
+
+	// put file content to screen
+	echo $file->getContent();
     }
 
 
