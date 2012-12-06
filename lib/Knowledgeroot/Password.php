f84901794d20145d28254f2aa3eea8ba0f781d46
lordlamer <lordlamer@lordlamer.de>
2012-12-06 23:33:27 +0100

    * added documentation

--- lib/Knowledgeroot/Password.php
+++ lib/Knowledgeroot/Password.php
@@ -1,8 +1,32 @@
 <?php
 /**
+ * Knowledgeroot
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://knowledgeroot.org/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@knowledgeroot.org so we can send you a copy immediately.
+ *
+ * @category   Knowledgeroot
+ * @package    Knowledgeroot_Password
+ * @copyright  Copyright (c) 2012 Frank Habermann
+ * @license    http://knowledgeroot.org/license/new-bsd     New BSD License
+ * @version    $Id:$
+ */
+
+/**
  * password class for knowledgeroot
  *
- * @author fhabermann
+ * @author     fhabermann
+ * @category   Knowledgeroot
+ * @package    Knowledgeroot_Version
+ * @copyright  Copyright (c) 2012 Frank Habermann
+ * @license    http://knowledgeroot.org/license/new-bsd     New BSD License
  */
 class Knowledgeroot_Password {
     /**
