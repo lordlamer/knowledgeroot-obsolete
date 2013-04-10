a7fca4d1caf7cf66e734c21abcbb67124af31677
lordlamer <lordlamer@lordlamer.de>
2013-04-10 22:59:46 +0200

    * using switch * added mysql to handle true and false

--- lib/Knowledgeroot/Db.php
+++ lib/Knowledgeroot/Db.php
@@ -4,20 +4,30 @@ class Knowledgeroot_Db {
 	public static function true() {
 		$db = Knowledgeroot_Registry::get('db');
 
-		if($db instanceof Zend_Db_Adapter_Pdo_Sqlite || $db instanceof Zend_Db_Adapter_Sqlsrv) {
-			return 1;
-		} else {
-			return 'true';
+		switch($db) {
+			case ($db instanceof Zend_Db_Adapter_Pdo_Sqlite):
+			case ($db instanceof Zend_Db_Adapter_Sqlsrv):
+			case ($db instanceof Zend_Db_Adapter_Pdo_Mysql):
+			case ($db instanceof Zend_Db_Adapter_Mysqli):
+				return 1;
+
+			default:
+				return 'true';
 		}
 	}
 
 	public static function false() {
 		$db = Knowledgeroot_Registry::get('db');
 
-		if($db instanceof Zend_Db_Adapter_Pdo_Sqlite || $db instanceof Zend_Db_Adapter_Sqlsrv) {
-			return 0;
-		} else {
-			return 'false';
+		switch($db) {
+			case ($db instanceof Zend_Db_Adapter_Pdo_Sqlite):
+			case ($db instanceof Zend_Db_Adapter_Sqlsrv):
+			case ($db instanceof Zend_Db_Adapter_Pdo_Mysql):
+			case ($db instanceof Zend_Db_Adapter_Mysqli):
+				return 0;
+
+			default:
+				return 'false';
 		}
 	}
 }
