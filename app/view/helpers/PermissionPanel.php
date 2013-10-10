e10b00121ef636c87357e5d4aa1c050343f13ec3
lordlamer <lordlamer@lordlamer.de>
2013-10-10 17:07:53 +0200

    * added option to enable default rights for active user in permission panel

--- app/view/helpers/PermissionPanel.php
+++ app/view/helpers/PermissionPanel.php
@@ -8,12 +8,21 @@ class Zend_View_Helper_PermissionPanel extends Zend_View_Helper_Abstract {
 	 * @param array $actions
 	 * @param array $config
 	 * @return string
+	 *
+	 * config options:
+	 * bool show_save_button - show save button so that stuff will be saved per ajax - default: false
+	 * bool add_acl_on_form_submit - should acl be submitted on form submit as value - default: false
+	 * bool add_user_permissions - add full permissions for the user itself if permissions are empty - default: false
 	 */
 	public function permissionPanel($name, $actions, $config = null) {
 	    $view = new Zend_View();
 
 	    $view->name = $name;
 	    $view->actions = $actions;
+
+	    // get actual userid
+	    $session = new Zend_Session_Namespace('user');
+	    $view->userId = $session->id;
 
 	    if(isset($config['show_save_button']) && $config['show_save_button']) {
 		$view->showSaveButton = true;
@@ -25,6 +34,12 @@ class Zend_View_Helper_PermissionPanel extends Zend_View_Helper_Abstract {
 		$view->addAclOnFormSubmit = true;
 	    } else {
 		$view->addAclOnFormSubmit = false;
+	    }
+
+	    if(isset($config['add_user_permissions']) && $config['add_user_permissions']) {
+		$view->addUserPermissions = true;
+	    } else {
+		$view->addUserPermissions = false;
 	    }
 
 	    // available roles
